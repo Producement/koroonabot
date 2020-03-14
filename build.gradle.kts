1c0ea92189fac53037a5f947a0ac6aab23c60dc0
Erko Risthein <erko@risthein.ee>
2020-03-14 22:44:14 +0200

    Fix build on java8

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.producement"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 val developmentOnly by configurations.creating
 configurations {
