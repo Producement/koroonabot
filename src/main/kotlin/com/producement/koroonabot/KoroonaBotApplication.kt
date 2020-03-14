50d8721a16aa1e85e6dfbbd114988a724cc3f30c
Erko Risthein <erko@risthein.ee>
2020-03-14 23:01:32 +0200

    Fix running on heroku

--- src/main/kotlin/com/producement/koroonabot/KoroonaBotApplication.kt
+++ src/main/kotlin/com/producement/koroonabot/KoroonaBotApplication.kt
@@ -1,13 +1,23 @@
 package com.producement.koroonabot
 
+import com.kreait.slack.broker.autoconfiguration.credentials.CredentialsProvider
+import com.kreait.slack.broker.autoconfiguration.credentials.EnvironmentVariableCredentialsProvider
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-class KoroonaBotApplication
+class KoroonaBotApplication {
+
+    @Bean
+    fun slackCredentialsProvider(): CredentialsProvider {
+        return EnvironmentVariableCredentialsProvider()
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<KoroonaBotApplication>(*args)
+    runApplication<KoroonaBotApplication>(*args)
 }
+
