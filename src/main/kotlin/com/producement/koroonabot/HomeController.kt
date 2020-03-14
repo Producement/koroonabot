255a4631417008a7618f2b755326044b569ad86b
Erko Risthein <erko@risthein.ee>
2020-03-15 00:02:12 +0200

    Fix redirect

--- src/main/kotlin/com/producement/koroonabot/HomeController.kt
+++ src/main/kotlin/com/producement/koroonabot/HomeController.kt
@@ -2,6 +2,7 @@ package com.producement.koroonabot
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 class HomeController {
@@ -12,7 +13,7 @@ class HomeController {
     }
 
     @GetMapping("/install")
-    fun install(): String {
-        return "redirect:https://slack.com/oauth/authorize?client_id=222663112231.1002854044022&scope=incoming-webhook,bot"
+    fun install(): RedirectView {
+        return RedirectView("https://slack.com/oauth/authorize?client_id=222663112231.1002854044022&scope=incoming-webhook,bot")
     }
 }
