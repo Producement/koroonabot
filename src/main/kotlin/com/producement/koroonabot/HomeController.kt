d1a9471ed17a200155f3014543c70b2067681b74
Erko Risthein <erko@risthein.ee>
2020-03-14 22:23:40 +0200

    Remove test code

--- src/main/kotlin/com/producement/koroonabot/HomeController.kt
+++ src/main/kotlin/com/producement/koroonabot/HomeController.kt
@@ -4,15 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HomeController(
-  private val slackService: SlackService,
-    private val terviseametWebsiteScraper: TerviseametWebsiteScraper
-) {
+class HomeController {
 
-  @GetMapping("/")
-  fun home(): String {
-    val latestData = terviseametWebsiteScraper.getLatestData()
-    slackService.sendAll(latestData)
-    return "KoroonaBot installeeritud."
-  }
+    @GetMapping("/")
+    fun home(): String {
+        return "KoroonaBot installeeritud."
+    }
 }
