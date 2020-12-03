cd823efb855753d8f416ca85feb3c4389e8bd9ce
Erko Risthein <erko@risthein.ee>
2020-12-03 15:38:47 +0200

    Add logging

--- src/main/kotlin/com/producement/koroonabot/slack/SlackService.kt
+++ src/main/kotlin/com/producement/koroonabot/slack/SlackService.kt
@@ -3,7 +3,10 @@ package com.producement.koroonabot.slack
 import com.kreait.slack.api.SlackClient
 import com.kreait.slack.api.contract.jackson.group.chat.PostMessageRequest
 import com.producement.koroonabot.slack.team.TeamRepository
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
+
+private val log = KotlinLogging.logger {}
 
 @Service
 class SlackService(
@@ -14,10 +17,15 @@ class SlackService(
   fun sendAll(message: String) {
     val teams = teamRepository.findAll()
     for (team in teams) {
-      slackClient.chat()
-          .postMessage(team.bot.accessToken)
-          .with(PostMessageRequest(text = message, channel = team.incomingWebhook!!.channel))
-          .invoke()
+      val result = slackClient.chat()
+        .postMessage(team.bot.accessToken)
+        .with(PostMessageRequest(text = message, channel = team.incomingWebhook!!.channel))
+        .invoke()
+      if (result.wasFailure()) {
+        log.error("Error sending to Slack: ${result.failure}")
+      } else {
+        log.info("Response from Slack: ${result.success}")
+      }
     }
   }
 }
