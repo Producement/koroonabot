16be5dd47b54643a8333bb73b09dc5fe16bed703
Erko Risthein <erko@producement.com>
2021-12-01 23:22:40 +0700

    Fix heroku plain jar file confusion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
 	id("org.springframework.boot") version "2.6.1"
@@ -49,4 +50,12 @@ tasks.withType<KotlinCompile> {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
 	}
+}
+
+tasks.withType<BootJar> {
+	archiveClassifier.set("boot")
+}
+
+tasks.withType<Jar>() {
+	archiveClassifier.set("")
 }
